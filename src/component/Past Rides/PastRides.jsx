4877580715e70deb4d65587cb97666df91547d4c
Muhammad Usman <usman.latif762@gmail.com>
2023-08-18 07:25:28 +0500

    updated

--- src/component/Past Rides/PastRides.jsx
+++ src/component/Past Rides/PastRides.jsx
@@ -1,16 +1,14 @@
-
-
 import { useState } from "react";
 import styles from './DragAndDropBoxes.module.css';
 import LeftBox from "./LeftBox";
 
 
 const PastRides = () => {
-    const [rightBoxItems, setRightBoxItems] = useState(["usman", "latif", "khan", "sb"]);
+    const [rightBoxItems, setRightBoxItems] = useState(LeftBox);
     const [leftBoxItems, setLeftBoxItems] = useState([]);
 
     const handleDragStart = (event, item) => {
-        event.dataTransfer.setData("text/plain", item);
+        event.dataTransfer.setData("text/plain", item.text);
     };
 
     const handleDragOver = (event) => {
@@ -19,13 +17,19 @@ const PastRides = () => {
 
     const handleDrop = (event) => {
         event.preventDefault();
-        const droppedItem = event.dataTransfer.getData("text/plain");
+        const droppedItemtext = event.dataTransfer.getData("text/plain");
 
-        // Remove the dropped item from the right box
-        setRightBoxItems(prevItems => prevItems.filter(item => item !== droppedItem));
+        const droppedItem = rightBoxItems.find(item => item.text === droppedItemtext)
 
-        // Add the dropped item to the left box
-        setLeftBoxItems(prevItems => [...prevItems, droppedItem]);
+        if (droppedItem) {
+
+            // Remove the dropped item from the right box
+            setRightBoxItems(prevItems => prevItems.filter(item => item !== droppedItem));
+
+            // Add the dropped item to the left box
+            setLeftBoxItems(prevItems => [...prevItems, droppedItem]);
+        }
+
     };
 
     return (
@@ -38,14 +42,25 @@ const PastRides = () => {
                 >
                     {leftBoxItems.map((item, index) => (
                         <div key={index} className={styles.draggableItem}>
-                            <div className={styles.container}>
+                            <div className={styles.rightdraggableItem}>
 
-                                <div className="one">box 1</div>
-                                <div className="one"> box 1</div>
+                                <div className={styles.rightBox}>{item.text}</div>
+                                {/* <div className={styles.rightBox}>{item.text}</div> */}
+
 
                             </div>
                         </div>
                     ))}
+                    {/* {leftBoxItems.slice(2).map((item, index) => (
+                        <div key={index} className={styles.draggableItem}>
+                            <div className={styles.rightdraggableItem}>
+
+                                <div className={styles.rightBox}>{item.text}</div>
+
+
+                            </div>
+                        </div>
+                    ))} */}
                 </div>
                 <div className={styles.box}>
                     {rightBoxItems.map((item, index) => (
@@ -53,9 +68,10 @@ const PastRides = () => {
                             key={index}
                             draggable
                             onDragStart={(e) => handleDragStart(e, item)}
-                            className={styles.draggableItem}
+                            className={styles.rightdraggableItem}
                         >
-                            {item}
+                            <div className={styles.rightBox}>{item.text}</div>
+
                         </div>
                     ))}
                 </div>
